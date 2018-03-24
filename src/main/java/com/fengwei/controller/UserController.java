19d1545f7aa558673caaecf332993417e76ef37b
fengwei <dde20011@hotmail.com>
2018-03-24 16:24:34 +0800

    update2

--- src/main/java/com/fengwei/controller/UserController.java
+++ src/main/java/com/fengwei/controller/UserController.java
@@ -4,9 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class UserController {
-    @RequestMapping("/hello1")
+    @RequestMapping("/hello")
     public String HelloWorld(){
         return "testtt11111";
     }
